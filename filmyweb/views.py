84b720a79399416b7bff4dc2c2e6d9a77d43e878
czarnyxtimon <czarnyxtimon@mail.com>
2024-08-28 14:52:47 +0200

    Operacje na obiektach ORM

--- filmyweb/views.py
+++ filmyweb/views.py
@@ -8,5 +8,18 @@ def wszystkie_filmy(request):
     # return HttpResponse("<h1>To jest nasz pierwszy test</h1>")
     # test = ["avatar", "titanic"]
     wszystkie = Film.objects.all()
+    # jeden = Film.objects.get(tytul='Titanic')
+    # jeden = Film.objects.filter(tytul='Titanic')
+
+    # Film.objects.create(tytul='Jakis1', rok=2020, opis='jakis5')
+
+    # film = Film.objects.get(id=5)
+    # film.tytul = 'Terminal'
+    # film.save()
+
+    # film.delete()
+    # lub
+    # Film.objects.get(id=5).delete()
+
     return render(request, 'filmy.html', {'filmy': wszystkie})
 
