a7d255f9df4295fd1a5b00a0c6bdbdef1e82f560
czarnyxtimon <czarnyxtimon@mail.com>
2024-08-28 13:50:53 +0200

    Dynamiczny template, przekazanie obiektow do strony i listowanie

--- filmyweb/views.py
+++ filmyweb/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Film
 
 # Create your views here.
 
 def wszystkie_filmy(request):
     # return HttpResponse("<h1>To jest nasz pierwszy test</h1>")
-    return render(request, 'filmy.html')
+    # test = ["avatar", "titanic"]
+    wszystkie = Film.objects.all()
+    return render(request, 'filmy.html', {'filmy': wszystkie})
+
