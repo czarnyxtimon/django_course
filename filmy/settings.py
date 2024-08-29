b694c249d3ed0377186592a8b9452ed77e9c4a1e
czarnyxtimon <czarnyxtimon@mail.com>
2024-08-29 10:24:25 +0200

    23. Login i logout

--- filmy/settings.py
+++ filmy/settings.py
@@ -122,6 +122,11 @@ STATICFILES_DIRS = ['moje_static']
 MEDIA_URL = 'media/'
 MEDIA_ROOT = 'moje_media'
 
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'wszystkie'
+
+# LOGOUT_REDIRECT_URL = 'login'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- filmy/urls.py
+++ filmy/urls.py
@@ -18,10 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('filmy/', include('filmyweb.urls'))
+    path('filmy/', include('filmyweb.urls')),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
     # wersja na prod -> https://docs.djangoproject.com/en/5.1/howto/static-files/deployment/
-    # wersja na development -> https://docs.djangoproject.com/en/5.1/howto/static-files/#serving-files-uploaded-by-a-user-during-development
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # wersja na development -> https://docs.djangoproject.com/en/5.1/howto/static-files/#serving-files-uploaded-by-a-user-during-development
